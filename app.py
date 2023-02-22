5c5572bca720506faf56e24739726026c11d894e
elior <elior1001@gmail.com>
2023-02-22 19:38:32 +0200

    created app.py with .git ignore, create general name of the functions

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Index page
+@app.route('/', methods=['GET'])
+def index():
+    return 'soon be implemented'
+
+# gets image from this person does not exists
+def get_random_image():
+    return "soon implemented"
+
+
+def get_image_info_from_aws():
+    return "soon implemented"
+
+def send_info_to_chat_gpt():
+    return "soon implemented"
+
+def render_result():
+    return "soon implemented" 
