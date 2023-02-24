2d1702b3dfa395c62455569ab53685fa07db0eb2
elior <elior1001@gmail.com>
2023-02-24 19:11:42 +0200

    chat gpt responds with a promt, and updated requiremnts

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import requests
 import boto3
 import json
 from bs4 import BeautifulSoup
+import os
+import openai
 
 app = Flask(__name__)
 
@@ -72,13 +74,13 @@ def get_image_info_from_aws(photo):
     with open(photo, 'rb') as image:
             response = client.detect_faces(Image={'Bytes': image.read()},Attributes=['ALL'])
     
-    print('Detected faces for ' + photo)    
+    #print('Detected faces for ' + photo)    
     for faceDetail in response['FaceDetails']:
-        print('The detected face is around ' + str(int(faceDetail['AgeRange']['Low']) + int(faceDetail['AgeRange']['High']) / 2 ))
+        #print('The detected face is around ' + str(int(faceDetail['AgeRange']['Low']) + int(faceDetail['AgeRange']['High']) / 2 ))
         age = str(int(faceDetail['AgeRange']['Low']) + int(faceDetail['AgeRange']['High']) / 2 )
-        print("gender: " + str(faceDetail['Gender']['Value']) + " with " + str(faceDetail['Gender']['Confidence']) + "%" )
+        #print("gender: " + str(faceDetail['Gender']['Value']) + " with " + str(faceDetail['Gender']['Confidence']) + "%" )
         gender = str(faceDetail['Gender']['Value'])
-        print("smile: " + str(faceDetail['Smile']['Value']) + " with " + str(faceDetail['Smile']['Confidence']) + "%" )
+        #print("smile: " + str(faceDetail['Smile']['Value']) + " with " + str(faceDetail['Smile']['Confidence']) + "%" )
         smile = str(faceDetail['Smile']['Value'])
         #print(json.dumps(faceDetail, indent=4, sort_keys=True)) ##### to print the whole list
     
@@ -87,8 +89,30 @@ def get_image_info_from_aws(photo):
     image_data["smile"]     = smile
     return image_data
 
-def send_info_to_chat_gpt():
-    return "soon implemented"
+# Data about person is a dict:
+# {
+#age: "", gender: , smileing
+#}
+def send_info_to_chat_gpt(data_about_person):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    is_smiling = "smiling" if data_about_person["smile"] == "True" else "not smiling"
+    
+    promt_to_chatGpt= f"Please generate a background for a {data_about_person['gender']} character who is {data_about_person['age']} years old and is {is_smiling} in a portrait . Please provide information about his name job education, hobbies, personality, hometown, and background. posible to add  give the response as a json"
+    # posible to add  give the response as a json
+
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt = promt_to_chatGpt,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    # print("hey")
+    print(response.choices[0].text)
+    return response.choices[0].text
+    
 
 def render_result():
     return "soon implemented" 
@@ -96,5 +120,6 @@ def render_result():
 
 
 if __name__ == '__main__':
+    send_info_to_chat_gpt(get_image_info_from_aws("images/random-face.jpg"))
     app.run(host='0.0.0.0',debug=True)
 
