9c53998f3e5fd928945b7c11937d3b99620b2c35
dvir pashut <pashutdvir@gmail.com>
2023-02-28 12:14:12 +0200

    first version of login is working

--- app.py
+++ app.py
@@ -54,13 +54,7 @@ def request_loader(request):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if flask.request.method == 'GET':
-        return '''
-               <form action='login' method='POST'>
-                <input type='text' name='email' id='email' placeholder='email'/>
-                <input type='password' name='password' id='password' placeholder='password'/>
-                <input type='submit' name='submit'/>
-               </form>
-               '''
+        return render_template("login.html")
 
     email = flask.request.form['email']
     if email in users and flask.request.form['password'] == users[email]['password']:
