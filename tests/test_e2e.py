90a333ce388c0a360fb470b3893facafc5ac9141
elior <elior1001@gmail.com>
2023-03-04 22:20:10 +0200

    put tests inside tests folder

---
+++ tests/test_e2e.py
@@ -0,0 +1,16 @@
+import pytest
+import requests
+from bs4 import BeautifulSoup
+import re
+
+ENDPOINT = "http://localhost:80"
+
+# Test the ability of the application to generate an image and information and display it on the persona route
+def test_persona_route():
+    response = requests.get(f"{ENDPOINT}/persona")
+    # parsing the response 
+    soup = BeautifulSoup(response.text, "html.parser")
+    img = soup.find("img", {"id": "avatar"})
+    print(img)
+    assert re.match("static/images/.*", img["src"])
+
---
+++ tests/test_prod_e2e.py
@@ -0,0 +1,45 @@
+import pytest
+import requests
+from bs4 import BeautifulSoup
+import re
+import os
+import subprocess
+
+username = os.environ.get("APP_USER")
+password = os.environ.get("APP_PASSWORD")
+
+
+
+ENDPOINT = "http://localhost:80"
+LIMIT = 5
+
+def test_limitter():
+    for i in range(LIMIT):
+        response = requests.get(f"{ENDPOINT}/persona")
+        #assert response.status_code == 200 # untill gebeta is ok this is like this
+    
+    response = requests.get(f"{ENDPOINT}/persona")
+    print(response)
+    assert response.status_code == 429
+
+def test_login():
+    payload = {
+    "email": username,
+    "password": password
+
+    }
+
+    payload2 = {
+    "email": "STUFF",
+    "password": "TO TESTS"
+    }
+
+    response = subprocess.check_output(['curl', '-s', '-o', '/dev/null', '-w', '%{http_code}', ENDPOINT + '/admin'])
+    response = int(response.decode().strip())
+    assert response == 302
+
+    response = requests.post(f"{ENDPOINT}/login" , data=payload2)
+    assert response.status_code == 401
+
+    response = requests.post(f"{ENDPOINT}/login" , data=payload)
+    assert response.status_code == 200
