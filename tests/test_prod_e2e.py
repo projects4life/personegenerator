ad8dacf1b12b11336d590c102e4726bd5496c717
elior <elior1001@gmail.com>
2023-03-07 22:53:25 +0200

    test before merge #2 #FULL-E2E

--- tests/test_prod_e2e.py
+++ tests/test_prod_e2e.py
@@ -13,14 +13,14 @@ password = os.environ.get("APP_PASSWORD")
 ENDPOINT = "http://localhost:80"
 LIMIT = 5
 
-# def test_limitter():
-#     for i in range(LIMIT):
-#         response = requests.get(f"{ENDPOINT}/persona")
-#         #assert response.status_code == 200 # untill gebeta is ok this is like this
+def test_limitter():
+    for i in range(LIMIT):
+        response = requests.get(f"{ENDPOINT}/persona")
+        #assert response.status_code == 200 # untill gebeta is ok this is like this
     
-#     response = requests.get(f"{ENDPOINT}/persona")
-#     print(response)
-#     assert response.status_code == 429
+    response = requests.get(f"{ENDPOINT}/persona")
+    print(response)
+    assert response.status_code == 429
 
 def test_login():
     payload = {
